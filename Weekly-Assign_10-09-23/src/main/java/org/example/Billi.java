f568155344635c916b88eb910e6c0a6c19cd4b31
Avadhesh Shukla <avadheshshukla1636@gmail.com>
2023-09-10 15:26:19 +0530

    geekster_Assignments_10/09/2023

---
+++ Weekly-Assign_10-09-23/src/main/java/org/example/Billi.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class Billi {
+    private String color;
+
+    public Billi() {
+        this.color="brownwhite";
+        System.out.println("cat object create ..........");
+    }
+
+//default  print
+
+
+    public Billi(String color) {
+        System.out.println("Billi color is :"+color);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
